f288d4d6b3c3a58c133c58a7f9e1a80926107628
Shubham-S151 <shubhamsharma15104@gmail.com>
2026-04-15 23:39:08 +0530

    Update 2_Monitoring.py

---
+++ streamlit_app/pages/2_Monitoring.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+class DataMonitoring:
+    def __init__(self):
+        pass
+
+class ModelMonitoring:
+    def __init__(self):
+        pass
+
+class ModelInfo:
+    def __init__(self):
+        pass
+
